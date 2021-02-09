ee3e6d8bd1712073905aafa53c84b1ad3d0b0e13
continuousRiven <732313118@qq.com>
2021-02-09 20:16:21 +0800

    completed lab3

--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -70,4 +70,14 @@ public class IntListTest {
       * to add a main method. See ArithmeticTest.java for an
       * example. */
 
+    @Test
+    public void testReverse() {
+        IntList A = IntList.of(1, 2, 3, 4, 5, 6);
+        IntList exp = IntList.of(6, 5, 4, 3, 2, 1);
+        IntList B = null;
+        assertEquals(exp,IntList.reverse(A));
+        assertNotEquals(A, IntList.of(1, 2, 3, 4, 5, 6));
+        assertEquals(IntList.reverse(B), null);
+    }
+
 }
